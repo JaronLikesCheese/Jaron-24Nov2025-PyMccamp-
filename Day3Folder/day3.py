6e670116bafc349c1b770a3beb419bbdb768a8a4
JaronLikesCheese <jaron0199@gmail.com>
2025-11-26 08:15:04 +0000

    Hi

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -1,3 +1,5 @@
+import random
+
 # Write all your codes for Day 3 here.
 # COMMENT out the previous task before going on to the next task
 print("hello from day3")
@@ -5,30 +7,47 @@ print("hello from day3")
 ########################################################################
 # Task 1:
 
+# name=input("What is your name?")
+# title=input("What is your title?")
+# command=input("What is your command?")
+# print(title+" "+name+" commands the peasants to "+command)
 
 
 ########################################################################
 # Task 2:
+# 
 
+# ########################################################################
+# # Task 3:
 
-
-########################################################################
-# Task 3:
-
+# NumberOne=int(input("What is your 1st number?"))
+# NumberTwo=int(input("What is your 2nd number?"))
+# print(NumberOne+NumberTwo)
 
 
 ########################################################################
 # Task 4:
 
 
-
 ########################################################################
 # Task 5:
-
+# age1=input("What is John's age?")
+# age2=input("What is Dylan's age?")
+# if age2<age1:
+#     print("John is older than Dylan.")
+# else:
+#     print("Dylan is younger than John.")
 
 
 ########################################################################
 # Task 6:
+# real_password=("Ch0col@t3")
+# user=input("Enter Password    ")
+# if user==real_password:
+#     print("Access Granted")
+# else:
+#     print("Access denied.This device will self-destruct.")
+
 
 
 
@@ -36,10 +55,22 @@ print("hello from day3")
 # Task 7:
 
 
+# for count in range(10):
+#     print(random.randint(1,100))
+       
+
 
 ########################################################################
 # Task 8:
+Num1=(random.randint(1,10))
+Num2=(random.randint(1,10))
 
+Student=int(input("What is "+str(Num1)+"+"+str(Num2)+"?"))
+answer=Num1+Num2
+if Student==answer:
+    print("You are very smart.")
+else:
+    print("GO TO THE PRINCIPAL'S OFFICE NOW!!")
 
 
 ########################################################################
