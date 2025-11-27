5726c1dd66c7def76c66dfc10f45387ad75e9441
JaronLikesCheese <jaron0199@gmail.com>
2025-11-27 07:21:23 +0000

    day 4

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -4,11 +4,37 @@ print("hello from day4")
 
 ########################################################################
 # Task 1:
+# counter=0
+# while counter <10:
+#     print(counter)
+#     counter=counter+1
+
+# counter=5
+# while counter<35:
+#     print(counter)
+#     counter=counter+1
+
+# counter=50
+# while counter>=1:
+#     print (counter)
+#     counter=counter-1
 
 
 
 ########################################################################
 # Task 2:
+
+
+Riddle=input("Answer this riddle. What question can't you answer yes to?")
+answer="Are you asleep yet?"
+
+
+
+while Riddle !=answer:
+    print("Try again.")    
+    Riddle=input("Answer this riddle. What question can't you answer yes to?")
+
+print("Yes,very impressive.")
 
 
 
