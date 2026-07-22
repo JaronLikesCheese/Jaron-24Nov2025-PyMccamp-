5af6cb5711ed7500096d56a8e4690c407f1fabe1
JaronLikesCheese <jaron0199@gmail.com>
2026-07-22 12:33:44 +0000

    Jul 22, 2026, 12:33 PM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -30,7 +30,7 @@ answer="Are you asleep yet?"
 
 
 
-while Riddle !b=answer:
+while Riddle !=answer:
     print("Try again.")    
     Riddle=input("Answer this riddle. What question can't you answer yes to?")
 
